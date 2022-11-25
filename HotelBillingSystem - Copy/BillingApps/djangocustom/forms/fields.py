47b7a15c3179f0af2152e8150ed712aef7394de9
Shankar Balaji <72439750+funnyguy369@users.noreply.github.com>
2022-11-25 18:28:31 +0530

    Delete HotelBillingSystem - Copy/BillingApps/djangocustom directory
    Unwanted Folder

--- HotelBillingSystem - Copy/BillingApps/djangocustom/forms/fields.py
+++
@@ -1,4 +0,0 @@
-# Django imports
-from django.forms.fields import Field
-from django.utils.translation import ugettext_lazy as _
-
--- HotelBillingSystem - Copy/BillingApps/djangocustom/forms/widgets.py
+++
@@ -1,6 +0,0 @@
-from django.forms.widgets import Input
-
-
-class SearchInput(Input):
-    input_type = 'search'
-    template_name = 'django/forms/widgets/text.html'
